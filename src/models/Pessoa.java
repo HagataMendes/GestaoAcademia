d822cf5a3b65467e2aea87723a8ff5f13a8414da
HagataMendes <hagatamendes2017@gmail.com>
2025-04-05 14:27:43 -0300

    ProjetoAcademiaPOO

---
+++ src/models/Pessoa.java
@@ -0,0 +1,22 @@
+package models;
+//HerançaPOO
+
+abstract class Pessoa {
+    protected String nomePessoa;
+    protected int idadePessoa;
+
+    public Pessoa(String nome, int idade) {
+        this.nomePessoa = nome;
+        this.idadePessoa = idade;
+    }
+
+    public String getNome() {
+        return nomePessoa;
+    }
+
+    public int getIdade() {
+        return idadePessoa;
+    }
+
+    public abstract String getDescricao();
+}
