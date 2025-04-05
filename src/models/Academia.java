f0c7de324d6663609ba41c45b2d280c3d7769e56
HagataMendes <hagatamendes2017@gmail.com>
2025-04-05 15:46:21 -0300

    Novas classes

--- src/models/Academia.java
+++ src/models/Academia.java
@@ -1,4 +1,24 @@
 package models;
 
-public class Academia {
+import java.util.ArrayList;
+
+class Academia {
+
+     private String nomeAcademia;
+     private ArrayList<Pessoa> pessoas;
+
+    public Academia(String nomeAcademia) {
+        this.nomeAcademia = nomeAcademia;
+        this.pessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(Pessoa pessoa) {
+        pessoas.add(pessoa);
+    }
+    public void listarPessoas() {
+        System.out.println("Lista de Pessoas na Academia:");
+        for (Pessoa pessoa : pessoas) {
+            System.out.println(pessoa.getDescricao());
+        }
+    }
 }
