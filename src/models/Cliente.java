e7cab7626f71c8cbac147c04751eeb3bdae3737a
HagataMendes <hagatamendes2017@gmail.com>
2025-04-05 15:14:21 -0300

    Nova Classe

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,4 +1,19 @@
 package models;
 
-public class Cliente {
+class Cliente extends Pessoa {
+    private String planoCliente;
+
+    public Cliente(String nome, int idade, String plano) {
+        super(nome, idade);
+        this.planoCliente = plano;
+    }
+
+    public String getPlano() {
+        return planoCliente;
+    }
+
+    @Override
+    public String getDescricao() {
+        return "Cliente: " + nomePessoa + ", Idade: " + idadePessoa + ", Plano: " + planoCliente;
+    }
 }
